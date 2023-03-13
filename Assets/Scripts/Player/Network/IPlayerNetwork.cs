0aca9078cdf737905fad600a64d4a76487ae03f4
Léo Ménard <leo.menard@epitech.eu>
2023-03-13 07:31:48 +0100

    Change the way of register things in PlayerNetwork

--- Assets/Scripts/Player/Network/IPlayerNetwork.cs
+++ Assets/Scripts/Player/Network/IPlayerNetwork.cs
@@ -1,9 +1,25 @@
-﻿using Player.Behaviour;
+﻿using UnityEngine;
+using Network;
+using Player.Behaviour;
+using Player.Information;
 
 namespace Player.Network
 {
     public interface IPlayerNetwork
     {
+
+        #region var sync
+
+        public APlayerInfos PlayerInfos { get; set; }
+
+        #endregion
+
+        #region non sync var
         public APlayerBehaviour PlayerBehaviour { get; set; }
+        public PlayerPrefab[] PlayerPrefabs { get; set; }
+        public GameObject ActualPlayer { get; set; }
+
+        #endregion
+
     }
 }
