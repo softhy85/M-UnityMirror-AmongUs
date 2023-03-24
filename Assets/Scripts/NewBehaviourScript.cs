4fcfa342fc43e48e962d9ef6134c5d5f93f3c157
MinMatth-Magi <desrues.matthieu@gmail.com>
2023-03-24 10:38:23 +0100

    Island map
    map-1 (might be oversized)
    
    Update URP.png
    
    map v2

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+    }
+
+    //! Important - formatted as bold.
+    //? Question - colored red.
+    //x Removed - formatted as strikeout.
+    //TODO: Task - colored light orange.
+    //!? WAT!? - colored purple.
+    private void OnMouseDown()
+	{
+        Debug.Log("Target Clicked !");
+	}
+
+}
